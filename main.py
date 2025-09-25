7c9e1c0ddaa685decd1de345f6938eda52a29c07
Abhishek <abhishek21shah@gmail.com>
2025-09-25 01:45:14 -0400

    Generate email with images attached

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import streamlit as st
 import streamlit.components.v1 as components
 from email.message import EmailMessage
 from fpdf import FPDF
-import tempfile
 
 msg = EmailMessage()
+# msg.make_alternative()
+# images_pdf = EmailMessage()
 st.write("# Email Generator")
 
 uploaded_files = st.file_uploader(
     "Upload images", accept_multiple_files=True, type=["jpg", "png", "jpeg"]
 )
+
+for uploaded_file in uploaded_files:
+    st.image(uploaded_file)
 
 recepient = st.text_input("What is the patient's name?", key="name")
 
@@ -46,39 +50,29 @@ if path_to_html is not False:
             html_data = f.read()
     components.html(html_data, height=400, scrolling=True)
 
-pdf_bytes = None
-if uploaded_files:
-    pdf = FPDF()
-    pdf.set_auto_page_break(auto=True, margin=15)
-    # Add a page with some text at the top
-    pdf.add_page()
-    pdf.set_font("Arial", size=14)
-    pdf.cell(0, 10, f"{recipient} Treatment Plan Images", ln=True, align="C")
-    pdf.ln(10)
-     
-    for uploaded_file in uploaded_files:
-        st.image(uploaded_file)
-
-        file_bytes = uploaded_file.read()
-        pdf.image(file_bytes.name, x=10, y=30, w=pdf.w - 20)
-
-        # Attach to email
-        msg.add_attachment(
-            file_bytes,
-            maintype="image",
-            subtype=uploaded_file.type.split("/")[-1],
-            filename=uploaded_file.name,
-        )
-
 msg['From'] = 'abhishek21shah@gmail.com'#'drshah.waterfrontdentistry@gmail.com'
 msg['To'] = recepient
 
 #msg['text'] = templates['Permissions']
-msg.set_content(html_data, subtype='html')
-# st.write(msg)
+msg.set_content("Please see the attached treatment plan and images.")
+msg.add_alternative(html_data, subtype='html')
+
 # if st.checkbox("Preview email file"):
 #     eml_string = msg.as_string()
 #     st.text_area("Email Preview (.eml format)", eml_string, height=300)
+
+for uploaded_file in uploaded_files:
+    # st.image(uploaded_file)
+
+    file_bytes = uploaded_file.read()
+
+    # Attach to email
+    msg.add_attachment(
+        file_bytes,
+        maintype="image",
+        subtype=uploaded_file.type.split("/")[-1],
+        filename=uploaded_file.name,
+    )
 
 msg['X-Unsent'] = '1'
 
